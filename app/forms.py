b865edf12e682deb42e18e0e062e24883bfbf7d1
Jesse Reitz <jessereitz1@gmail.com>
2017-12-26 20:12:34 -0600

    Create login and signup views and forms.

--- app/forms.py
+++ app/forms.py
@@ -9,9 +9,15 @@
 #####     IMPORTS     #####
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
     submit = SubmitField('Sign In')
+
+class SignupForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired(), EqualTo('confirm_password', message='Passwords must match')])
+    confirm_password = PasswordField('confirm password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,26 @@
 #####     routes.py - WEBTODO     #####
 #######################################
 
-
+from flask import render_template, flash, redirect, url_for
 from app import app
-from flask import render_template
+from app.forms import LoginForm, SignupForm
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title="Sign In")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}'.format(form.username.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='login', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        flash('Create user {}'.format(form.username.data))
+        return redirect(url_for('index'))
+    return render_template('signup.html', tile='sign up', form=form)
