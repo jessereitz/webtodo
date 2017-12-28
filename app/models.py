5df5e12e8c1a60e073efe811ca3e4f28e4327ed7
Jesse Reitz <jessereitz1@gmail.com>
2017-12-27 19:33:03 -0600

    Update menu bar. Display login and signup if logged out; logout if logged in.

--- app/models.py
+++ app/models.py
@@ -11,13 +11,35 @@ from app import db
 
 class User(db.Model):
     __tablename__ = 'users'
+
+    # Database Schema
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password = db.Column(db.String(64))
     tasks = db.relationship('Task', backref='user', lazy='dynamic')
 
+    # flask-login properties
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)
+        except NameError:
+            return str(self.id)
+
     def __repr__(self):
         return '<User %s>' % (self.username)
+
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- app/routes.py
+++ app/routes.py
@@ -2,26 +2,62 @@
 #####     routes.py - WEBTODO     #####
 #######################################
 
-from flask import render_template, flash, redirect, url_for
-from app import app
+from flask import render_template, flash, redirect, url_for, session, request, g
+from flask_login import login_user, logout_user, current_user, login_required
+from app import app, db, loginManager
 from app.forms import LoginForm, SignupForm
+from app.models import User, Task
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+@loginManager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="Sign In")
+    
+    return render_template('index.html', title="home")
+
+@app.route('/protected')
+@login_required
+def protected():
+    flash("You got it, kid")
+    return redirect(url_for('index'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}'.format(form.username.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if user.password == form.password.data:
+                login_user(user)
+                flash('Logged in successfully!')
+                return redirect(url_for('index'))
+            else:
+                form.password.errors.append("Incorrect password.")
+        else:
+            flash("User not found.")
     return render_template('login.html', title='login', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignupForm()
     if form.validate_on_submit():
-        flash('Create user {}'.format(form.username.data))
-        return redirect(url_for('index'))
+        if not User.query.filter_by(username=form.username.data).first():
+            newUser = User(username=form.username.data, password=form.password.data)
+            db.session.add(newUser)
+            db.session.commit()
+            flash("New user " + newUser.username + " successfully created!")
+            return redirect(url_for('login'))
+        else:
+            form.username.errors.append("Username taken.")
     return render_template('signup.html', tile='sign up', form=form)
